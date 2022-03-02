c23d9a82c71c95f7cf1b033b6db4ed20f70a8933
Daniel Sateler <62968964+satelerd@users.noreply.github.com>
2022-03-02 18:00:33 -0300

    Update Generador-de-leyes.py

--- Generador-de-leyes.py
+++ Generador-de-leyes.py
@@ -2,13 +2,13 @@ import os
 import openai
 
 # Select the theme or leave blank for auto theme generation
-theme = "" 
+theme = "Ley que aprueba el tabaco en los festivales de musica" 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
   engine="text-davinci-001",
-  prompt="Lo siguiente es un la ley creada en 2022 sobre un tema en específico en chile.\n###\nTema: ",
+  prompt=f"Lo siguiente es un la ley creada en 2022 sobre un tema en específico en chile.\n###\nTema: {theme}",
   temperature=0.7,
   max_tokens=64,
   top_p=1,
